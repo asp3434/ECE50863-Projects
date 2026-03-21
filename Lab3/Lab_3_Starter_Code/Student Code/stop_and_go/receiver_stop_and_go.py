f15bf6531fc10dc5d3d13f3262b02edbf195058d
asp3434 <andrew.prescott.2019@gmail.com>
2026-03-21 14:13:03 -0600

    interim checkpoint for Lab3

---
+++ Lab3/Lab_3_Starter_Code/Student Code/stop_and_go/receiver_stop_and_go.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+from monitor import Monitor
+import sys
+import os
+
+# Config File
+import configparser
+
+n_packets = 0
+def num_packets(file_size, chunk_size):
+    global n_packets
+    if (file_size - chunk_size) < chunk_size:
+        return n_packets + 1
+    else:
+        n_packets += 1
+        if n_packets == 100:
+            chunk_size = chunk_size - 1
+        elif n_packets == 10:
+            chunk_size = chunk_size -1
+            
+        file_size = file_size - chunk_size
+        return num_packets(file_size, chunk_size)
+
+def send_ack(recvd_pkt):
+    # print(f'Sending ACK...')
+    message = recvd_pkt + ' ACK'.encode("utf-8")
+    recv_monitor.send(sender_id, message)
+
+if __name__ == '__main__':
+    print("Receiver starting up!")
+    config_path = sys.argv[1]
+    
+    # Initialize receiver monitor
+    recv_monitor = Monitor(config_path, 'receiver')
+    
+    # Parse config file
+    cfg = configparser.RawConfigParser(allow_no_value=True)
+    cfg.read(config_path)
+    sender_id = int(cfg.get('sender', 'id'))
+    file_to_send = cfg.get('nodes', 'file_to_send')
+    max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE')) - 5
+    
+    #initialize write locations and the size of the file to send
+    write_location = cfg.get('receiver', 'write_location')
+    log_file = cfg.get('receiver', 'log_file')
+    file_size = os.path.getsize(file_to_send)
+    
+    # determine number of chunks to be sent
+    chunk_size = max_packet_size -2
+    n_packets = num_packets(file_size, chunk_size)
+    
+    # exchange messages
+    logged_packets = 0
+    recv_chunks = []
+    while logged_packets < n_packets+1:
+        # receive messages
+        addr, data = recv_monitor.recv(max_packet_size)
+        # print(f'Receiver: Got message from id {addr}: {data}')
+        
+        logged_packets += 1
+        seq_str, chunk = data.split(b'\n', 1)
+        i = int(seq_str)
+        # print(seq_str, i, len(seq_str))
+        print('\n')
+        print(seq_str, len(seq_str))
+        print(len(chunk))
+        
+        if i != logged_packets:
+            logged_packets -= 1
+            send_ack(f'{i}\n'.encode("utf-8"))
+        
+        else:
+            # append the received data to a list
+            recv_chunks.append(chunk)
+            send_ack(f'{i}\n'.encode("utf-8"))
+
+    # write the received data to a file
+    with open(write_location, 'w') as file:
+        file.write(b''.join(recv_chunks).decode('utf-8')) 
+        
+    # Exit. Make sure the receiver ends before the sender. send_end will stop the emulator.
+    recv_monitor.recv_end(write_location, sender_id)
---
+++ Lab3/Lab_3_Starter_Code/Student Code/stop_and_go/sender_stop_and_go.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+from monitor import Monitor
+import sys
+import os
+import math
+import socket
+import time
+
+# Config File
+import configparser
+
+def send_pkt(pkt):
+    seq_num = pkt.split(b'\n')[0].decode()
+    print(f'Sending packet {seq_num}...')
+    # print('\n')
+    # print(pkt)
+    # print('\n')
+    print(len(pkt))
+    send_monitor.send(receiver_id, pkt)
+
+if __name__ == '__main__':
+    print("Sender starting up!")
+    config_path = sys.argv[1]
+    
+    # Initialize sender monitor
+    send_monitor = Monitor(config_path, 'sender')
+    
+    # Parse config file
+    cfg = configparser.RawConfigParser(allow_no_value=True)
+    cfg.read(config_path)
+    receiver_id = int(cfg.get('receiver', 'id'))
+    file_to_send = cfg.get('nodes', 'file_to_send')
+    max_packet_size = int(cfg.get('network', 'MAX_PACKET_SIZE')) - 5
+    
+    #the size of the file to send
+    log_file = cfg.get('sender', 'log_file')
+    file_size = os.path.getsize(file_to_send)
+    
+    # determine number of chunks to be sent
+    chunk_size = max_packet_size -2
+    
+    # set timeout
+    prop_delay = float(cfg.get('network', 'PROP_DELAY'))
+    bandwidth = float(cfg.get('network', 'LINK_BANDWIDTH'))
+    trans_delay = max_packet_size / bandwidth
+    timeout = prop_delay*2 + trans_delay*2
+    send_monitor.socketfd.settimeout(timeout)
+    
+    # Exchange messages
+    sent_pkts = []
+    i = 0
+    with open(file_to_send, 'rb') as file:
+        while True:
+            if i <= 8:
+                chunk_size = max_packet_size -2
+            elif i <= 98:
+                chunk_size = max_packet_size - 3
+            else:
+                chunk_size = max_packet_size - 4
+                
+            chunk = file.read(chunk_size)
+            if not chunk:
+                break
+            else:
+                i +=1
+                pkt = f'{i}\n'.encode("utf-8") + chunk
+                sent_pkts.append(pkt)
+                send_pkt(pkt) # change "chunk" later on to packet when it includes a header
+                
+                while True:
+                    try:
+                        addr, data = send_monitor.recv(max_packet_size)
+                        # print(f'Sender: Got response from id {addr}: {data}')
+                        
+                        ack_recv = int(data.split()[0])
+                        if ack_recv == i:
+                            break
+                        else:
+                            continue
+                        
+                    except socket.timeout:
+                        send_pkt(pkt)
+                                 
+    print(f"Total packets sent: {len(sent_pkts)}")
+    
+    # Exit. Make sure the receiver ends before the sender. send_end will stop the emulator.
+    time.sleep(1)
+    send_monitor.send_end(receiver_id)
